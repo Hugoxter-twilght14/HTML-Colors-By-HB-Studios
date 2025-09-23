53dbcca00fe7d15b6ab0c61a45db30f80dc173bd
Twilight14 <borrazgonzalezhugo@gmail.com>
2025-09-22 23:54:15 -0600

    Ajuste minimo en diseño

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,6 @@ import Paleta from './selector-paleta/Paleta'
 export default function Home() {
   return (
     <div>
-      <h1 className='text-center text-2xl text-white mt-10'>COLORES HTML</h1>
-      <div>
-        <p className='text-white mt-10 text-center mb-10'>
-          Todos los colores con código disponibles para tus 
-          proyectos de programación o de diseño gráfico
-          totalmente gratis!!
-        </p>
-      </div>
       <Paleta/>
     </div>
   )
